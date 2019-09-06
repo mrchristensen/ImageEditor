b46985151b2be3020833e6cfde5c3f54f78c884e
Matt <mr.christensen@gmail.com>
2019-09-06 17:33:19 -0600

    Basic Read implemented

--- src/editor/ReadClass.java
+++ src/editor/ReadClass.java
@@ -1,4 +1,16 @@
 package editor;
 
-public class ReadClass {
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class ReadClass{
+    public void processFile(File myFile) throws IOException {
+        Scanner scanner = new Scanner(myFile);
+        scanner.useDelimiter("((#[^\\n]*\\n)|(\\s+))+");
+        while(scanner.hasNext()) {
+            String str = scanner.next();
+            // Do something with the String
+        }
+    }
 }
