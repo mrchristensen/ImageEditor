482aad3c7f44d23c53387bc614eb5006b1600a3c
Matt <mr.christensen@gmail.com>
2019-09-07 14:43:39 -0600

    Implemented the emboss transform

--- src/editor/Transformer.java
+++ src/editor/Transformer.java
@@ -29,6 +29,32 @@ public class Transformer {
     }
 
     public PPMImage emboss(PPMImage myImage) {
+        for (int i = myImage.height - 1; i >= 0; i--) { //Starts are bottom right corner of the "pixel array"/image
+            for (int j = myImage.width - 1; j >= 0; j--) {
+                int v;
+
+                if (j > 0 && i > 0) { //Pixels that have a top left neighbour
+                    int redDiff = myImage.pixels[j][i].red - myImage.pixels[j - 1][i - 1].red;
+                    int greenDiff = myImage.pixels[j][i].green - myImage.pixels[j - 1][i - 1].green;
+                    int blueDiff = myImage.pixels[j][i].blue - myImage.pixels[j - 1][i - 1].blue;
+                    int maxDifference = findMaxDiff(redDiff, greenDiff, blueDiff); //Find the absolute biggest difference between the three separate colors
+
+                    v = maxDifference + 128;
+                    if (v < 0) {
+                        v = 0;
+                    } else if (v > 255) {
+                        v = 255;
+                    }
+                } else { //Pixels that don't have a top left neighbour
+                    v = 128;
+                }
+
+                myImage.pixels[j][i].red = v;
+                myImage.pixels[j][i].green = v;
+                myImage.pixels[j][i].blue = v;
+
+            }
+        }
         return myImage;
     }
 
@@ -41,7 +67,7 @@ public class Transformer {
                 int totalBlueValues = 0;
                 int hPos = j;
 
-                while(hPos < myImage.width & hPos <= j+blurLength-1) { //Checks to make sure that we have more pixels to read and we haven't already hit our desired range
+                while(hPos < myImage.width && hPos <= j+blurLength-1) { //Checks to make sure that we have more pixels to read and we haven't already hit our desired range
                     numColorValues++;
                     totalRedValues += myImage.pixels[hPos][i].red;
                     totalGreenValues += myImage.pixels[hPos][i].green;
@@ -57,6 +83,28 @@ public class Transformer {
         }
 
         return myImage;
+    }
+
+    private int findMaxDiff(int x, int y, int z) {
+        if(Math.abs(x) == Math.abs(y) && Math.abs(y) == Math.abs(z)){ //If they are all the same then return the first (red) value
+            return x;
+        } else if(Math.abs(x) > Math.abs(y) && Math.abs(x) > Math.abs(z)) { //If x is biggest
+            return x;
+        } else if(Math.abs(y) > Math.abs(x) && Math.abs(y) > Math.abs(z)) { //If y is biggest
+            return y;
+        } else if(Math.abs(z) > Math.abs(x) && Math.abs(z) > Math.abs(y)){ //If z is biggest
+            return z;
+        } else if(Math.abs(x) > Math.abs(z) && Math.abs(y) > Math.abs(z)){ //If x and y are bigger than z, take x
+            return x;
+        } else if (Math.abs(x) > Math.abs(y) && Math.abs(z) > Math.abs(y)){ //If x and z are bigger than y, take x
+            return x;
+        } else if (Math.abs(y) > Math.abs(x) && Math.abs(z) > Math.abs(x)){ //If y and z are bigger than x, take y
+            return y;
+        } else{
+            System.out.println("findMaxDiff function in incomplete in it's cases\nx: " + Math.abs(x) + ", y: " + Math.abs(y) + ", z: " + Math.abs(z));
+            return -1;
+        }
+
     }
 
 }
