3b01f9422eb10af50934768ee7141f2f966b5cf0
Matt <mr.christensen@gmail.com>
2019-09-07 15:07:29 -0600

    Optimized write functionality with StringBuilder to decrease time to run

--- src/editor/WriteClass.java
+++ src/editor/WriteClass.java
@@ -7,18 +7,19 @@ import java.io.IOException;
 public class WriteClass {
 
     public void writeImage(String filename, PPMImage myImage) throws FileNotFoundException, IOException {
-        System.out.println("Begin a write on: " + filename);
-
-        String dataStream = "P3\n" + myImage.width + "\n" + myImage.height + "\n255\n";
+        System.out.println("Begin gathering data into string");
+        StringBuilder dataStream = new StringBuilder();
+         dataStream.append("P3\n" + myImage.width + "\n" + myImage.height + "\n255\n");
         for (int i = 0; i < myImage.height; i++) {
             for (int j = 0; j < myImage.width; j++) {
-                dataStream += "" + myImage.pixels[j][i].red + " " + myImage.pixels[j][i].green + " " + myImage.pixels[j][i].blue + "  ";
+                dataStream.append("" + myImage.pixels[j][i].red + " " + myImage.pixels[j][i].green + " " + myImage.pixels[j][i].blue + "  ");
             }
-            dataStream += "\n";
+            dataStream.append("\n");
         }
 
+        System.out.println("Begin a write on: " + filename);
         FileOutputStream fos = new FileOutputStream(filename);
-        fos.write(dataStream.getBytes());
+        fos.write(dataStream.toString().getBytes());
         fos.close();
     }
 
